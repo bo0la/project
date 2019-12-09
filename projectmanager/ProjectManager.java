26068d81735eb76052d90b1850ddf8660c399736
bo0la <boolaraouf@yahoo.com>
2019-12-09 22:01:43 +0200

    in comit

---
+++ projectmanager/ProjectManager.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectmanager;
+
+/**
+ *
+ * @author berlin
+ */
+public class ProjectManager extends SupeEmployee{
+   private int projectComletion;
+    String report;
+    private String projectName;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+    
+    public void teamLeader setProjectCompletion(teamLeader T){
+        
+    }
+    public void teamLeader makeEmployeeReport(String s,teamLeader T){
+        
+    }
+    int getProjectCompletion()
+    {
+        
+        return projectComletion;
+    }
+    if (projectName=="k"){
+    
+}
+ 
+}
---
+++ projectmanager/SupeEmployee.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectmanager;
+
+/**
+ *
+ * @author berlin
+ */
+public class SupeEmployee {
+    private String id,employeeName,email,password;
+    private long mobile;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+    
+    
+}
